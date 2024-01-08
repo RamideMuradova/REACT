539409cbe6e66c29250b7fe585e50c3993e848f6
Ramide Muradova <ramidemuradova003@gmail.com>
2024-01-08 01:20:02 -0800

    lifecycling

--- 05.lifecycling/src/App.jsx
+++ 05.lifecycling/src/App.jsx
@@ -4,6 +4,7 @@ import Header from "./layouts/header/Header";
 import About from "./pages/about-page/About";
 import Home from "./pages/home-page/Home";
 import Products from "./pages/products/Products";
+import ProductsTable from "./components/products-table/ProductsTable";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="about" element={<About />} />
           <Route path="products" element={<Products />} />
+          <Route path="products-table" element={<ProductsTable/>}/>
         </Routes>
         <Footer />
       </div>
